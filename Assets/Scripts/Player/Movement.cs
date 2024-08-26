d24e0d1d4f45156ad0720a8a2881c72369517982
K D K Haranatha Pradyumna <143993158+HaranathKolluru@users.noreply.github.com>
2024-08-27 03:42:46 +0530

    INIT

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody player;
+    private float movespeed = 30f;
+    // private float turnSpeed = 100f;
+    private int score = 0;
+    [SerializeField] Text Score;
+    void Start()
+    {
+        player = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  if(transform.position.y<0)
+        {   
+          SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        }
+       transform.Translate(movespeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f,0f);
+       transform.Translate(0f,0f,movespeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {   if(collision.gameObject!=null)
+        {
+            Destroy surface = collision.gameObject.GetComponent<Destroy>();
+            if (!surface.getVariable())
+            {
+                surface.SetVariable();
+                score++;
+                Score.text = "Score :" + score;
+            }
+        }
+    }
+}
